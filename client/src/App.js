df6414b74f71fafe16512b8a84a63e5e3a0fba36
Shaqilah <NU0001LI@E.NTU.EDU.SG>
2024-10-08 16:56:16 +0800

    just trying

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+// main font used
+import "@fontsource/poppins"; 
+import "@fontsource/poppins/700.css"; 
+import "@fontsource/poppins/400-italic.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
