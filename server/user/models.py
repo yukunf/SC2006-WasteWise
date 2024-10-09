1780a1f017391009b7a1fa37659b323380c609b3
Shiro <fengyukunfyk@gmail.com>
2024-10-09 16:41:20 +0800

    add backend user process

--- server/user/models.py
+++ server/user/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+import uuid
+
+
+class Role(models.TextChoices):
+    GENERAL = 'general', 'General'
+    COLLECTOR = 'collector', 'Collector'
+    REGULATOR = 'regulator', 'Regulator'
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=30)
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.GENERAL)
+
+    def __str__(self):
+        return self.username
