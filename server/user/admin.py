fb439ec6e69120b7544e82d3bc98d443fd421ae0
Shiro <fengyukunfyk@gmail.com>
2024-10-13 11:59:19 +0800

    Rebuild the model using inheritance from AbstractUser

--- server/user/admin.py
+++ server/user/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import User, CollectorUser
+from .models import GeneralUser  #, CollectorUser
 
-@admin.register(User)
+
+@admin.register(GeneralUser)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'firstName', 'lastName', 'email', 'role')  # Customize as needed
 
-@admin.register(CollectorUser)
-class CollectorUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'firstName', 'lastName', 'email', 'collector_id')  # Customize as needed
-
-
+# @admin.register(CollectorUser)
+# class CollectorUserAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'firstName', 'lastName', 'email', 'collector_id')  # Customize as needed
