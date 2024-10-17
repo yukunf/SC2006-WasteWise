c6c1ec0509ac76a4087d853665da98efad5c8507
Shiro <fengyukunfyk@gmail.com>
2024-10-17 15:33:49 +0800

    collector app init

--- server/collector/models.py
+++ server/collector/models.py
@@ -1,3 +1,13 @@
+# Create your models here.
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Collector(models.Model):
+    ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15, blank=True)
+    fax = models.CharField(max_length=15, blank=True)
+    licence = models.CharField(max_length=3)
+    suspended = models.BooleanField(default=False)
